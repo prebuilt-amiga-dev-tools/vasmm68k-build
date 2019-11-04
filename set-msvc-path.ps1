841b9c5b8c171230cdececf80f658206e4139ef3
Mikael Kalms <mikael@kalms.org>
2019-11-04 08:50:20 +0100

    Added detailed error reporting to set-msvc-path

--- set-msvc-path.ps1
+++ set-msvc-path.ps1
@@ -1,8 +1,20 @@
 
-$installationPath = & ((Get-Item "Env:ProgramFiles(x86)").Value + "\Microsoft Visual Studio\Installer\vswhere.exe") -prerelease -latest -property installationPath
-if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
-  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=x64 -host_arch=x64 -no_logo && set" | foreach-object {
-    $name, $value = $_ -split '=', 2
-    set-content env:\"$name" $value
+$vswherePath = ((Get-Item "Env:ProgramFiles(x86)").Value + "\Microsoft Visual Studio\Installer\vswhere.exe")
+if (Test-Path $vswherePath) {
+  $installationPath = & $vswherePath -prerelease -latest -property installationPath
+  if ($installationPath) {
+    if (Test-Path "$installationPath\Common7\Tools\vsdevcmd.bat") {
+      & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=x64 -host_arch=x64 -no_logo && set" | foreach-object {
+        $name, $value = $_ -split '=', 2
+        Set-Content env:\"$name" $value
+      }
+    } else {
+      Write-Error -Message "Visual Studio installation at $installationPath does not include vsdevcmd.bat" -Exception ([System.IO.FileNotFoundException]::New()) -ErrorAction Stop
+    }
+  } else {
+    Write-Error -Message "vswhere.exe did not find any Visual Studio installation" -Exception ([System.ApplicationException]::New()) -ErrorAction Stop
   }
+} else {
+  Write-Error -Message "Could not find VSWhere.exe in default location: $vswherePath" -Exception ([System.IO.FileNotFoundException]::New()) -ErrorAction Stop
 }
+
