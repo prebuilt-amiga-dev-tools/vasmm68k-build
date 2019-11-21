73d94f356a971e9beb663da415040c91b10cddc5
Mikael Kalms <mikael@kalms.org>
2019-11-21 22:14:45 +0100

    Debug printouts

--- set-wix-path.ps1
+++ set-wix-path.ps1
@@ -1,4 +1,10 @@
 # GitHub runners have a WIX environment variable which points out the WIX install location
 # See https://help.github.com/en/actions/automating-your-workflow-with-github-actions/software-installed-on-github-hosted-runners
 
+Write-Output "Adding Wix tools to environment..."
+
 $env:Path += ";${WIX}\bin";
+
+Write-Output $env:Path
+
+Write-Output "Done."
