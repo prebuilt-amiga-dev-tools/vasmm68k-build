cc84e31fe44755e47a1721e4ce754901a4f9e558
Mikael Kalms <mikael@kalms.org>
2019-12-04 08:15:55 +0100

    Invoke installers directly from nmake
    Don't launch installers from subshells.
    That makes them unable to update the environment
    variables -- in particular, the path will not be
    updated correctly.

--- NMake.mk
+++ NMake.mk
@@ -76,19 +76,21 @@ test-binaries:
 test-wix:
 	powershell -Command "$$ErrorActionPreference = 'Stop'; if (Test-Path $(BUILD_RESULTS_DIR)/temp) { Remove-Item -Force -Recurse $(BUILD_RESULTS_DIR)/temp }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; mkdir -Force $(BUILD_RESULTS_DIR)/temp"
-	powershell -Command "$$ErrorActionPreference = 'Stop'; & '$(BUILD_RESULTS_DIR)/vasmm68k-$(VASM_VERSION)-windows-installer.msi' /quiet; refreshenv"
+	"$(BUILD_RESULTS_DIR)\\vasmm68k-$(VASM_VERSION)-windows-installer.msi" /quiet
+	refreshenv
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vasmm68k_mot.exe -Fhunk -o '$(BUILD_RESULTS_DIR)/temp/test_mot.o' tests/test_mot.s; if ($$LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash 'tests/test_mot.o.expected').hash) -ne ((Get-FileHash '$(BUILD_RESULTS_DIR)/temp/test_mot.o').hash)) { throw 'vasmm68k_mot output does not match reference' }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vasmm68k_std.exe -Fhunk -o '$(BUILD_RESULTS_DIR)/temp/test_std.o' tests/test_std.s; if ($$LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash 'tests/test_std.o.expected').hash) -ne ((Get-FileHash '$(BUILD_RESULTS_DIR)/temp/test_std.o').hash)) { throw 'vasmm68k_std output does not match reference' }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vobjdump.exe 'tests/test_vobjdump.o' > '$(BUILD_RESULTS_DIR)/temp/test_vobjdump.dis'; if ($$LASTEXITCODE -ne 0) { throw }; if (Compare-Object -ReferenceObject (Get-Content 'tests/test_vobjdump.dis.expected') -DifferenceObject (Get-Content '$(BUILD_RESULTS_DIR)/temp/test_vobjdump.dis')) { throw 'vobjdump output does not match reference' }"
-	powershell -Command "$$ErrorActionPreference = 'Stop'; & msiexec /x '$(BUILD_RESULTS_DIR)/vasmm68k-$(VASM_VERSION)-windows-installer.msi' /quiet; refreshenv"
+	msiexec /x "$(BUILD_RESULTS_DIR)\\vasmm68k-$(VASM_VERSION)-windows-installer.msi" /quiet
+	refreshenv
 	powershell -Command "$$ErrorActionPreference = 'Stop'; Remove-Item -Force -Recurse $(BUILD_RESULTS_DIR)/temp"
 
 test-choco:
 	powershell -Command "$$ErrorActionPreference = 'Stop'; if (Test-Path $(BUILD_RESULTS_DIR)/temp) { Remove-Item -Force -Recurse $(BUILD_RESULTS_DIR)/temp }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; mkdir -Force $(BUILD_RESULTS_DIR)/temp"
-	powershell -Command "$$ErrorActionPreference = 'Stop'; & choco install -y '$(BUILD_RESULTS_DIR)/vasmm68k.$(VASM_VERSION).nupkg'; if ($$LASTEXITCODE -ne 0) { throw }"
+	choco install -y "$(BUILD_RESULTS_DIR)/vasmm68k.$(VASM_VERSION).nupkg"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vasmm68k_mot.exe -Fhunk -o '$(BUILD_RESULTS_DIR)/temp/test_mot.o' tests/test_mot.s; if ($$LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash 'tests/test_mot.o.expected').hash) -ne ((Get-FileHash '$(BUILD_RESULTS_DIR)/temp/test_mot.o').hash)) { throw 'vasmm68k_mot output does not match reference' }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vasmm68k_std.exe -Fhunk -o '$(BUILD_RESULTS_DIR)/temp/test_std.o' tests/test_std.s; if ($$LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash 'tests/test_std.o.expected').hash) -ne ((Get-FileHash '$(BUILD_RESULTS_DIR)/temp/test_std.o').hash)) { throw 'vasmm68k_std output does not match reference' }"
 	powershell -Command "$$ErrorActionPreference = 'Stop'; & vobjdump.exe 'tests/test_vobjdump.o' > '$(BUILD_RESULTS_DIR)/temp/test_vobjdump.dis'; if ($$LASTEXITCODE -ne 0) { throw }; if (Compare-Object -ReferenceObject (Get-Content 'tests/test_vobjdump.dis.expected') -DifferenceObject (Get-Content '$(BUILD_RESULTS_DIR)/temp/test_vobjdump.dis')) { throw 'vobjdump output does not match reference' }"
-	powershell -Command "$$ErrorActionPreference = 'Stop'; & choco uninstall vasmm68k; if ($$LASTEXITCODE -ne 0) { throw }"
+	choco uninstall vasmm68k
 	powershell -Command "$$ErrorActionPreference = 'Stop'; Remove-Item -Force -Recurse $(BUILD_RESULTS_DIR)/temp"
