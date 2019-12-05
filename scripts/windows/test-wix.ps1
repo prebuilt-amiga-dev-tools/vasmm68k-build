c5f4c5158e67b2ae82c2a3d1a51ef18a664998c0
Mikael Kalms <mikael@kalms.org>
2019-12-05 13:12:37 +0100

    Wrap .msi invocation with Start-Process

--- scripts/windows/test-wix.ps1
+++ scripts/windows/test-wix.ps1
@@ -10,11 +10,11 @@ if (Test-Path "${BUILD_RESULTS_DIR}/temp")
 
 mkdir -ErrorAction Stop -Force "${BUILD_RESULTS_DIR}/temp"
 Write-Output "Lastexitcode pre: ${LASTEXITCODE}"
-& "${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi" /quiet; if ($LASTEXITCODE -ne 0) { Write-Output "Lastexitcode: ${LASTEXITCODE}"; throw }
+Start-Process -ErrorAction Stop -Wait -NoNewWindow msiexec -ArgumentList "/i","${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi","/quiet"
 refreshenv
 & vasmm68k_mot.exe -Fhunk -o "${BUILD_RESULTS_DIR}/temp/test_mot.o" "tests/test_mot.s"; if ($LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash "tests/test_mot.o.expected").hash) -ne ((Get-FileHash "${BUILD_RESULTS_DIR}/temp/test_mot.o").hash)) { throw 'vasmm68k_mot output does not match reference' }
 & vasmm68k_std.exe -Fhunk -o "${BUILD_RESULTS_DIR}/temp/test_std.o" "tests/test_std.s"; if ($LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash "tests/test_std.o.expected").hash) -ne ((Get-FileHash "${BUILD_RESULTS_DIR}/temp/test_std.o").hash)) { throw 'vasmm68k_std output does not match reference' }
 & vobjdump.exe "tests/test_vobjdump.o" > "${BUILD_RESULTS_DIR}/temp/test_vobjdump.dis"; if ($LASTEXITCODE -ne 0) { throw }; if (Compare-Object -ReferenceObject (Get-Content "tests/test_vobjdump.dis.expected") -DifferenceObject (Get-Content "${BUILD_RESULTS_DIR}/temp/test_vobjdump.dis")) { throw 'vobjdump output does not match reference' }
-& msiexec /x "${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi" /quiet; if ($LASTEXITCODE -ne 0) { throw }
+Start-Process -ErrorAction Stop -Wait -NoNewWindow msiexec -ArgumentList "/x","${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi","/quiet"
 refreshenv
 Remove-Item -ErrorAction Stop -Force -Recurse ${BUILD_RESULTS_DIR}/temp
