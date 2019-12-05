36ee0ec21cdf4c7669774dd5e003cf247fba706e
Mikael Kalms <mikael@kalms.org>
2019-12-05 13:04:16 +0100

    Added some debug logging

--- scripts/windows/test-wix.ps1
+++ scripts/windows/test-wix.ps1
@@ -9,7 +9,8 @@ if (Test-Path "${BUILD_RESULTS_DIR}/temp")
 }
 
 mkdir -ErrorAction Stop -Force "${BUILD_RESULTS_DIR}/temp"
-& "${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi" /quiet; if ($LASTEXITCODE -ne 0) { throw }
+Write-Output "Lastexitcode pre: ${LASTEXITCODE}"
+& "${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi" /quiet; if ($LASTEXITCODE -ne 0) { Write-Output "Lastexitcode: ${LASTEXITCODE}"; throw }
 refreshenv
 & vasmm68k_mot.exe -Fhunk -o "${BUILD_RESULTS_DIR}/temp/test_mot.o" "tests/test_mot.s"; if ($LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash "tests/test_mot.o.expected").hash) -ne ((Get-FileHash "${BUILD_RESULTS_DIR}/temp/test_mot.o").hash)) { throw 'vasmm68k_mot output does not match reference' }
 & vasmm68k_std.exe -Fhunk -o "${BUILD_RESULTS_DIR}/temp/test_std.o" "tests/test_std.s"; if ($LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash "tests/test_std.o.expected").hash) -ne ((Get-FileHash "${BUILD_RESULTS_DIR}/temp/test_std.o").hash)) { throw 'vasmm68k_std output does not match reference' }
