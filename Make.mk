7c6682ae85a79ac77d63d7230618c461353a25e3
Mikael Kalms <mikael@kalms.org>
2019-12-05 21:38:55 +0100

    clean, download, build done outside of debuild

--- Make.mk
+++ Make.mk
@@ -9,17 +9,33 @@ endif
 
 BUILD_RESULTS_DIR = build_results
 
-.PHONY: clean download build install uninstall
+.PHONY: clean download build package
 
-default: debuild package
+default: clean download build package
+
+.PHONY: install install-deb remove-deb release
 
 ######################################################################################
 # These build steps are intended to be invoked manually with make
 
-debuild:
-	sudo DEBUILD_DPKG_BUILDPACKAGE_OPTS="-r'fakeroot --faked faked-tcp' -us -uc" DEBUILD_LINTIAN_OPTS="-i -I --show-overrides" debuild --no-conf -us -uc
+clean:
+	rm -rf vasm
+	rm -f vasm.tar.gz
+	rm -rf $(BUILD_RESULTS_DIR)
 
-package:
+download:
+	wget -O vasm.tar.gz $(VASM_URL)
+	tar -xvf vasm.tar.gz
+	rm vasm.tar.gz
+
+build:
+	(cd vasm && make CPU=m68k SYNTAX=mot && chmod ugo+rx vasmm68k_mot)
+	(cd vasm && make CPU=m68k SYNTAX=std && chmod ugo+rx vasmm68k_std)
+
+package: package-deb
+
+package-deb:
+	sudo DEBUILD_DPKG_BUILDPACKAGE_OPTS="-r'fakeroot --faked faked-tcp' -us -uc" DEBUILD_LINTIAN_OPTS="-i -I --show-overrides" debuild --no-conf -us -uc
 	mkdir -p $(BUILD_RESULTS_DIR)
 	cp ../vasmm68k_$(VASM_VERSION)_amd64.deb $(BUILD_RESULTS_DIR)/vasmm68k_$(VASM_VERSION)_amd64.$(DISTRIBUTION).deb
 
@@ -39,20 +55,6 @@ remove-deb:
 ######################################################################################
 # These build steps will be invoked by the 'debuild' tool; they are referenced in the debian/rules file
 # They can also be invoked manually with make
-
-clean:
-	rm -rf vasm
-	rm -f vasm.tar.gz
-	rm -rf $(BUILD_RESULTS_DIR)
-
-download:
-	wget -O vasm.tar.gz $(VASM_URL)
-	tar -xvf vasm.tar.gz
-	rm vasm.tar.gz
-
-build:
-	(cd vasm && make CPU=m68k SYNTAX=mot && chmod ugo+rx vasmm68k_mot)
-	(cd vasm && make CPU=m68k SYNTAX=std && chmod ugo+rx vasmm68k_std)
 
 install:
 	mkdir -p $(DESTDIR)/usr/bin
