e250d24cad876a51e3a6c78ca405d27d021e3d51
Mikael Kalms <mikael@kalms.org>
2019-12-05 13:12:57 +0100

    Removed debug logging

--- scripts/windows/test-wix.ps1
+++ scripts/windows/test-wix.ps1
@@ -9,7 +9,6 @@ if (Test-Path "${BUILD_RESULTS_DIR}/temp")
 }
 
 mkdir -ErrorAction Stop -Force "${BUILD_RESULTS_DIR}/temp"
-Write-Output "Lastexitcode pre: ${LASTEXITCODE}"
 Start-Process -ErrorAction Stop -Wait -NoNewWindow msiexec -ArgumentList "/i","${BUILD_RESULTS_DIR}\\vasmm68k-${VASM_VERSION}-windows-installer.msi","/quiet"
 refreshenv
 & vasmm68k_mot.exe -Fhunk -o "${BUILD_RESULTS_DIR}/temp/test_mot.o" "tests/test_mot.s"; if ($LASTEXITCODE -ne 0) { throw }; if (((Get-FileHash "tests/test_mot.o.expected").hash) -ne ((Get-FileHash "${BUILD_RESULTS_DIR}/temp/test_mot.o").hash)) { throw 'vasmm68k_mot output does not match reference' }
