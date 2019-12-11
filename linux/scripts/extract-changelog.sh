0b65cb6d0e942ddd3314b50c07d719d4abb89a5a
Mikael Kalms <mikael@kalms.org>
2019-12-11 22:52:15 +0100

    Changelog parsing bugfixes

--- linux/scripts/extract-changelog.sh
+++ linux/scripts/extract-changelog.sh
@@ -4,8 +4,8 @@ VASM_VERSION=$1
 
 # Extract changelog as text file
 cp vasm/history build_results/changelog.txt
-awk "/- ${VASM_VERSION}/{flag=1} /^- /{flag=0} flag" vasm/history > build_results/changelog-for-version.txt
+awk "/^- ${VASM_VERSION}/{flag=1; next} /^$/{flag=0} flag" vasm/history > build_results/changelog-for-version.txt
 
 # Convert changelog to markdown
-sed "s/^- /# /g; s/^o /* /g; s/\\\\/\\\\\\\\/g; s/_/\\\\_/g; s/</\\\\</g; s/>/\\\\>/g; s/[/\\\\[/g; s/]/\\\\]/g" build_results/changelog.txt > build_results/changelog.md
-sed "s/^- /# /g; s/^o /* /g; s/\\\\/\\\\\\\\/g; s/_/\\\\_/g; s/</\\\\</g; s/>/\\\\>/g; s/[/\\\\[/g; s/]/\\\\]/g" build_results/changelog-for-version.txt > build_results/changelog-for-version.md
+sed "s/^- /# /g; s/^o /* /g; s/\\\\/\\\\\\\\/g; s/_/\\\\_/g; s/</\\\\</g; s/>/\\\\>/g; s/\\[/\\\\[/g; s/\\]/\\\\]/g" build_results/changelog.txt > build_results/changelog.md
+sed "s/^- /# /g; s/^o /* /g; s/\\\\/\\\\\\\\/g; s/_/\\\\_/g; s/</\\\\</g; s/>/\\\\>/g; s/\\[/\\\\[/g; s/\\]/\\\\]/g" build_results/changelog-for-version.txt > build_results/changelog-for-version.md
