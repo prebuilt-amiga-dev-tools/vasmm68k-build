e566ce537256ab3d42b02b0e2c7293833954caa8
Mikael Kalms <mikael@kalms.org>
2019-12-22 20:06:26 +0100

    Config.mk must not have quoted values (Windows incompatible)

--- Config.mk
+++ Config.mk
@@ -1,4 +1,4 @@
 
-VASM_URL = "http://server.owl.de/~frank/tags/vasm1_8f.tar.gz"
-VASM_DOC_URL = "http://sun.hasenbraten.de/vasm/release/vasm.pdf"
-VASM_VERSION = "1.8f"
+VASM_URL = http://server.owl.de/~frank/tags/vasm1_8f.tar.gz
+VASM_DOC_URL = http://sun.hasenbraten.de/vasm/release/vasm.pdf
+VASM_VERSION = 1.8f
