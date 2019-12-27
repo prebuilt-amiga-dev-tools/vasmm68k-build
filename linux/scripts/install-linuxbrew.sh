9ec066b8de0cf8ce0959bd57ea5b362357878b63
Mikael Kalms <mikael@kalms.org>
2019-12-27 16:19:59 +0100

    Also update .bashrc for linuxbrew

--- linux/scripts/install-linuxbrew.sh
+++ linux/scripts/install-linuxbrew.sh
@@ -1,5 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bashrc
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
